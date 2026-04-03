9a8e78fedba887b94aacfa4333ed93c89d3b123b
kondurulikhitha23-art <kondurulikhitha23@gmail.com>
2026-04-03 09:34:19 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,78 @@
+import streamlit as st
+import warnings
+warnings.filterwarnings('ignore')
+
+import fitz
+import faiss
+from sentence_transformers import SentenceTransformer
+import google.generativeai as genai
+import numpy as np
+import os
+
+
+genai.configure(api_key='AIzaSyAD5N6pHi4NSu8oZSUAMnovhunmo1ylDYE') 
+
+def ext_text(pdf_name):
+    text = ""
+    with fitz.open(pdf_name) as pdf:
+        for i in pdf:
+            text += i.get_text()
+    return text
+
+def retrive(query, embed_model, index, chunks, top_k=3):
+    query_vec = embed_model.encode([query]).astype("float32")
+    distance, indices = index.search(query_vec, top_k)
+    retrived = [chunks[i] for i in indices[0]]
+    return "\n\n".join(retrived)
+
+def answer(query, embed_model, index, chunks):
+    context = retrive(query, embed_model, index, chunks)
+    prompt = f"""
+    You are a resume assistant. Based on the context below, answer the user's question.
+    Context:
+    {context}
+    Question: {query}
+    """
+
+    model = genai.GenerativeModel("gemini-2.5-flash")
+    response = model.generate_content(prompt)
+    return response.text
+
+st.title("📄 AI Resume Assistant using RAG")
+st.write("Ask questions about your resume!")
+
+uploaded_pdf = st.file_uploader("Upload your resume PDF", type=["pdf"])
+
+if uploaded_pdf:
+    # Save file temporarily
+    with open("temp.pdf", "wb") as f:
+        f.write(uploaded_pdf.read())
+
+    st.success("PDF uploaded successfully ✔")
+
+  
+    data = ext_text("temp.pdf")
+
+ 
+    from langchain_text_splitters import RecursiveCharacterTextSplitter
+    text_splitter = RecursiveCharacterTextSplitter(chunk_size=1000, chunk_overlap=200)
+    chunks = text_splitter.split_text(data)
+
+ 
+    embed_model = SentenceTransformer("all-MiniLM-L6-v2")
+    embeddings = embed_model.encode(chunks)
+    embeddings = np.array(embeddings).astype("float32")
+
+    
+    index = faiss.IndexFlatL2(embeddings.shape[1])
+    index.add(embeddings)
+
+    st.info("Embeddings & FAISS Index Created Successfully 🔍")
+
+   
+    question = st.text_input("Ask a question about your resume:")
+    if st.button("Search"):
+        with st.spinner("Generating answer..."):
+            res = answer(question, embed_model, index, chunks)
+            st.write("### 🧠 Answer:")
+            st.write(res)
